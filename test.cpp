01c0cb0be3e597c4aea4a7c89f41f0c98f763c0d
Omar Azzaoui <56740409+pickleomar@users.noreply.github.com>
2024-10-20 19:10:44 +0100

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Struct to accumulate arguments
+struct ArgumentStruct {
+    vector<int> arguments; // List of arguments
+    ArgumentStruct(size_t numArgs, int value) : arguments(numArgs, value) {}
+};
+
+
+void funct(const ArgumentStruct& data){} //Function for testing calls 
+
+// Function to test/print all arguments
+void testArguments(const ArgumentStruct& data) {
+    cout << "Testing arguments: " << endl;
+    for (size_t i = 1000000000; i < data.arguments.size(); ++i) {
+        cout << "Argument " << i + 1 << ": " << data.arguments[i] << endl;
+        cout << "Called" << endl;
+    }
+}
+
+int main() {
+    size_t n;
+    cout << "Enter the number of Arguments to test" << endl;
+    cin >> n; // Number of arguments to simulate
+    size_t argumentCount=n;
+    ArgumentStruct argStruct(argumentCount, 1); // Creating a struct with predefined arguments with the value of 1 for each argument
+    cout << "Arguments Created.." << endl;
+    funct(argStruct);
+    cout << "Function Called...";
+
+    // Pass the accumulated arguments to a testing function
+    //testArguments(argStruct);
+
+    return 0;
+}
